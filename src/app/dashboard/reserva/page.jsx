9d3baf2977f2a196c221430529c23c3ea1fd03bd
Alejo <alejandrocantillop.ilusa@gmail.com>
2025-10-17 21:46:51 -0500

    First commit

---
+++ src/app/dashboard/reserva/page.jsx
@@ -0,0 +1,10 @@
+const ReservaPage = () => {
+  return (
+    <div>
+      <h1>Reserva de Citas</h1>
+      {/* Aquí puedes agregar la lógica para reservar citas */}
+    </div>
+  );
+};
+
+export default ReservaPage;
